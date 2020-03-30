aca03bc7392c1556b26cac6a7b5d8845706f6188
myzner <diziwii@gmail.com>
2020-03-30 18:55:52 +0300

    Updates

---
+++ precache-manifest.22f8ed823af8a8c4dd47db9d1cdc42ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48baf85ad590a02fe109ad682f6c1ff",
+    "url": "/streamlabs-test-task/index.html"
+  },
+  {
+    "revision": "c0cd3c2d56af9b540225",
+    "url": "/streamlabs-test-task/static/css/main.61dda9aa.chunk.css"
+  },
+  {
+    "revision": "5d829d9069c2df508330",
+    "url": "/streamlabs-test-task/static/js/2.112cb84f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/streamlabs-test-task/static/js/2.112cb84f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cd3c2d56af9b540225",
+    "url": "/streamlabs-test-task/static/js/main.237e7a5d.chunk.js"
+  },
+  {
+    "revision": "09fc6f7355b1237e08d8",
+    "url": "/streamlabs-test-task/static/js/runtime-main.8c8bb20e.js"
+  }
+]);
